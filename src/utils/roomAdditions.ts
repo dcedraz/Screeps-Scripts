5c78430719c567a7386bd1ee1c77c6ca56f79c03
dcedraz <dcedraz@salsify.com>
2025-07-20 00:50:02 +0100

    Fix bug on roomAdditions

--- src/utils/roomAdditions.ts
+++ src/utils/roomAdditions.ts
@@ -24,100 +24,104 @@ const allStructureTypes = [
   STRUCTURE_INVADER_CORE,
 ];
 
-Object.defineProperties(Room.prototype, {
-  sources: {
+// Guard all prototype extensions to avoid redefining on module reloads
+type RoomWithCache = Room & {
+  _sources?: Source[];
+  _mineral?: Mineral;
+  _myCreeps?: Creep[];
+  _enemyCreeps?: Creep[];
+  _structures?: { [type: string]: Structure[] };
+  _cSitesGrouped?: { [type: string]: ConstructionSite[] };
+  _cSites?: ConstructionSite[];
+  _droppedEnergy?: Resource[];
+};
+const roomProto = Room.prototype as RoomWithCache;
+if (!Object.getOwnPropertyDescriptor(roomProto, "sources")) {
+  Object.defineProperty(roomProto, "sources", {
     get() {
       if (this._sources) return this._sources;
-
       if (!this.memory.sourceIds) {
         this.memory.sourceIds = [];
-
         for (const source of this.find(FIND_SOURCES)) this.memory.sourceIds.push(source.id);
       }
-
       this._sources = [];
-
       for (const sourceId of this.memory.sourceIds)
-        this._sources.push(HelperFunctions.findObjectWithID(sourceId) as any);
-
+        this._sources.push(HelperFunctions.findObjectWithID(sourceId) as Source);
       return this._sources;
     },
-  },
-  mineral: {
+    configurable: true,
+  });
+}
+if (!Object.getOwnPropertyDescriptor(roomProto, "mineral")) {
+  Object.defineProperty(roomProto, "mineral", {
     get() {
       if (this._mineral) return this._mineral;
-
       return (this._mineral = this.find(FIND_MINERALS)[0]);
     },
-  },
-  myCreeps: {
+    configurable: true,
+  });
+}
+if (!Object.getOwnPropertyDescriptor(roomProto, "myCreeps")) {
+  Object.defineProperty(roomProto, "myCreeps", {
     get() {
       if (this._myCreeps) return this._myCreeps;
-
       return (this._myCreeps = this.find(FIND_MY_CREEPS));
     },
-  },
-  enemyCreeps: {
+    configurable: true,
+  });
+}
+if (!Object.getOwnPropertyDescriptor(roomProto, "enemyCreeps")) {
+  Object.defineProperty(roomProto, "enemyCreeps", {
     get() {
       if (this._enemyCreeps) return this._enemyCreeps;
-
       return (this._enemyCreeps = this.find(FIND_HOSTILE_CREEPS));
     },
-  },
-  structures: {
+    configurable: true,
+  });
+}
+if (!Object.getOwnPropertyDescriptor(roomProto, "structures")) {
+  Object.defineProperty(roomProto, "structures", {
     get() {
       if (this._structures) return this._structures;
-
-      // Construct storage of structures based on structureType
-
-      this._structures = {} as any;
-
-      // Make array keys for each structureType
-
+      this._structures = {};
       for (const structureType of allStructureTypes) this._structures[structureType] = [];
-
-      // Group structures by structureType
-
       for (const structure of this.find(FIND_STRUCTURES))
-        this._structures[structure.structureType].push(structure as any);
-
+        this._structures[structure.structureType].push(structure);
       return this._structures;
     },
-  },
-  cSitesGrouped: {
+    configurable: true,
+  });
+}
+if (!Object.getOwnPropertyDescriptor(roomProto, "cSitesGrouped")) {
+  Object.defineProperty(roomProto, "cSitesGrouped", {
     get() {
       if (this._cSitesGrouped) return this._cSitesGrouped;
-
-      // Construct storage of structures based on structureType
-
-      this._cSitesGrouped = {} as any;
-
-      // Make array keys for each structureType
-
+      this._cSitesGrouped = {};
       for (const structureType of allStructureTypes) this._cSitesGrouped[structureType] = [];
-
-      // Group cSites by structureType
-
       for (const cSite of this.find(FIND_MY_CONSTRUCTION_SITES))
         this._cSitesGrouped[cSite.structureType].push(cSite);
-
       return this._cSitesGrouped;
     },
-  },
-  cSites: {
+    configurable: true,
+  });
+}
+if (!Object.getOwnPropertyDescriptor(roomProto, "cSites")) {
+  Object.defineProperty(roomProto, "cSites", {
     get() {
       if (this._cSites) return this._cSites;
-
       return (this._cSites = this.find(FIND_MY_CONSTRUCTION_SITES));
     },
-  },
-  droppedEnergy: {
+    configurable: true,
+  });
+}
+if (!Object.getOwnPropertyDescriptor(roomProto, "droppedEnergy")) {
+  Object.defineProperty(roomProto, "droppedEnergy", {
     get() {
       if (this._droppedEnergy) return this._droppedEnergy;
-
       return (this._droppedEnergy = this.find(FIND_DROPPED_RESOURCES, {
-        filter: (resource) => resource.resourceType === RESOURCE_ENERGY,
+        filter: (resource: Resource) => resource.resourceType === RESOURCE_ENERGY,
       }));
     },
-  },
-} as PropertyDescriptorMap & ThisType<Room>);
+    configurable: true,
+  });
+}
